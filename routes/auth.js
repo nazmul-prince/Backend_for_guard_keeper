22799574c8377b22c05550f397faf5a0c589c1cd
nazmul-prince <nazmul.prince@groupbuilder.fi>
2019-08-09 10:39:43 +0600

    crud request for user

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,55 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const User = require('../models/User');
+const { check, validationResult } = require('express-validator');
+
 const router = express.Router();
 
 const getLoggedInUser = (req, res) => {
     res.send('Get logged in user');
 }
 
-const authUserGetToken = (req, res) => {
-    res.send('Auth user and get token');
+const authUserGetToken = async (req, res) => {
+
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+        let user = await User.findOne({ email });
+
+        if(!user) {
+            return res.status(400).json({msg: 'Invalid credentials' });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if(!isMatch) {
+            return res.status(400).json({ msg: 'Invalid credentials' });
+        }
+
+        const payload = {
+            user: {
+                id: user.id
+            }
+        };
+
+        jwt.sign(payload, config.get('jwtSecret'), {
+            expiresIn: 360000
+        }, (err, token) => {
+            if(err) throw err;
+            res.json({ token });
+        });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
 }
 
 // @route   POST api/auth
@@ -14,6 +57,10 @@ const authUserGetToken = (req, res) => {
 // @acess   Public
 router.get('/', getLoggedInUser);
 
-router.post('/', authUserGetToken);
+router.post('/', [
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Password is required').exists()
+],
+ authUserGetToken);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,8 +2,15 @@ const express = require('express');
 const users = require('./routes/users');
 const contacts = require('./routes/contacts');
 const auth = require('./routes/auth');
+const connectDB = require('./config/db');
 
 const app = express();
+
+// Connect Database
+connectDB();
+
+// Init middleware
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => res.send("Guard keeper"));
 
